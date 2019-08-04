8d0a2e46dd021c366b125e8a29ee75cbf6f2359f
dletk <duc.mtle@gmail.com>
2019-08-03 22:59:40 -0400

    Added base template for the program and generated home view

--- HTM_Season2/HTM_Season2/urls.py
+++ HTM_Season2/HTM_Season2/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Handle all requests related to user
     path("user/", include("userprofile.urls")),
+    # Using simple class-view to display the homepage (no need for creating an app)
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ HTM_Season2/khoidong/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KhoidongConfig(AppConfig):
+    name = 'khoidong'
