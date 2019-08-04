b99db3d6b831fbc2dbe8d0a09c75ec8d22d7ae7c
dletk <duc.mtle@gmail.com>
2019-08-04 16:04:44 -0400

    Adding fileType into the model so front-end can be displayed correctly

--- HTM_Season2/khoidong/models.py
+++ HTM_Season2/khoidong/models.py
@@ -9,31 +9,33 @@ def questionDirectoryPath(instance, filename):
     """
     return "khoidong/{}/{}".format(instance.questionID, filename)
 
-# Create your models here.
+
 class KhoiDongQuestion(models.Model):
     """
     Model for question in Khoi Dong round
     """
-    
+
     # The question ID in in this round
     questionID = models.IntegerField(primary_key=True,
-                                    verbose_name="Câu hỏi số")
+                                     verbose_name="Câu hỏi số")
 
     # The text content of the question
-    questionText = models.TextField(blank=True, 
+    questionText = models.TextField(blank=True,
                                     verbose_name="Nội dung câu hỏi")
 
     # The answer for this question
-    answer = models.CharField(max_length=256, blank=False, verbose_name="Đáp án")
+    answer = models.CharField(
+        max_length=256, blank=False, verbose_name="Đáp án")
 
     # Question file (for video/sound/image questions)
     file = models.FileField(verbose_name="File đính kèm",
-                            upload_to=questionDirectoryPath, 
+                            upload_to=questionDirectoryPath,
                             blank=True)
 
-    
+    fileType = models.CharField(max_length=10,
+                                verbose_name="Loại file",
+                                choices = [("sound", "Âm thanh"), ("video", "Video"), ("image", "Hình ảnh")],
+                                blank=True)
+
     def __str__(self):
         return "Khởi động: Câu hỏi {}".format(self.questionID)
-    
-
-
