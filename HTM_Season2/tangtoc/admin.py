ef19095cd5fd6591a67284c9ef036705ea99e4d7
dletk <duc.mtle@gmail.com>
2019-08-11 15:48:08 -0400

    Added model for field of question in tang toc

--- HTM_Season2/tangtoc/admin.py
+++ HTM_Season2/tangtoc/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TangTocQuestion
+from .models import TangTocQuestion, TangTocQuestionField
 
 # Register your models here.
 admin.site.register(TangTocQuestion)
+admin.site.register(TangTocQuestionField)
--- HTM_Season2/tangtoc/models.py
+++ HTM_Season2/tangtoc/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from userprofile.models import MyUser
 
 # Create your models here.
+
+
+class TangTocQuestionField(models.Model):
+    """
+    Model for field in Tang Toc round
+    """
+
+    # Name of the field to store in database
+    name = models.CharField(
+        max_length=50, primary_key=True, verbose_name="Tên lĩnh vực")
+
+    # Code to avoid problem with Vietnamese
+    code = models.CharField(max_length=30, blank=False, verbose_name="Tên không dấu")
+
+    # Keep track whether this field is used
+    used = models.BooleanField(default=False, verbose_name="Đã sử dụng")
+
+
 class TangTocQuestion(models.Model):
     """
     Model for questions in Tang Toc round
@@ -9,15 +27,16 @@ class TangTocQuestion(models.Model):
     # No need to create a question ID in this case, Django will generate a primary key for each question
 
     # The text content of the question
-    questionText = models.TextField(blank=False, verbose_name="Nội dung câu hỏi")
+    questionText = models.TextField(
+        blank=False, verbose_name="Nội dung câu hỏi")
 
-    FIELDS = [("Toan", "Toán"), ("Ly", "Lý"), ("Hoa", "Hoá")]
     # The field of this question
-    questionField = models.CharField(max_length=50, verbose_name="Lĩnh vực",
-                                     choices = FIELDS)
+    questionField = models.ForeignKey(TangTocQuestionField, on_delete=models.CASCADE,
+                                      verbose_name="Lĩnh vực")
 
     # The answer for this question
-    answer = models.CharField(max_length=255, blank=False, verbose_name="Đáp án")
+    answer = models.CharField(
+        max_length=255, blank=False, verbose_name="Đáp án")
 
     def __str__(self):
         return self.questionField + ": " + self.questionText
