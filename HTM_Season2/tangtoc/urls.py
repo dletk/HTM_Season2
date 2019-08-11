883dd75fa8d67d6345e527565d0807ca809ac309
dletk <duc.mtle@gmail.com>
2019-08-11 15:48:25 -0400

    Display available models to screen

--- HTM_Season2/tangtoc/urls.py
+++ HTM_Season2/tangtoc/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .forms import TangTocQuestionForm
-from .views import NewQuestion
+from .views import NewQuestion, getAvailableFields
 
 urlpatterns = [
     path("newQuestion/", NewQuestion.as_view(), name="newTangTocQuestion"),
+    path("getField/", getAvailableFields, name="tangtoc"),
 ]
--- HTM_Season2/tangtoc/views.py
+++ HTM_Season2/tangtoc/views.py
@@ -3,8 +3,11 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 from .forms import TangTocQuestionForm
+from .models import TangTocQuestion, TangTocQuestionField
 
 # Create your views here.
+
+
 class NewQuestion(CreateView):
     """
     Class-based view to handle creating a new question
@@ -23,3 +26,29 @@ class NewQuestion(CreateView):
         else:
             return render(request, template_name="home.html",
                           context={"message": "Xin lỗi, bạn không được phép truy cập tính năng này"})
+
+
+def getAvailableFields(request):
+    """
+    View to handle the request for get the current remaining available fields
+    """
+    availableFields = []
+
+    for field in TangTocQuestionField.objects.all():
+        if not field.used:
+            availableFields.append(field)
+
+    print(availableFields)
+    return render(request, template_name="tangtoc/tangtocField.html", context={"fields": availableFields,
+                                                               "numFields": range(len(availableFields))})
+
+
+def getNewQuestion(request, field):
+    """
+    Function to handle request to get a new set of questions
+
+    Params:
+        fields (string): The field of the set of questions
+    Return:
+        questions (QuerySet): The set of questiosn for this contestant
+    """
