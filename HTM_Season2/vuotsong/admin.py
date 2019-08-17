8272ed47e39e4c80b5083fb362e193473a2a63e6
doannguyenthanhluong@gmail.com <doannguyenthanhluong@gmail.com>
2019-08-18 02:02:04 +0900

    Register for VuotSong model

--- HTM_Season2/vuotsong/admin.py
+++ HTM_Season2/vuotsong/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import VuotSongQuestion, VuotSongAnswer
 
 # Register your models here.
+admin.site.register(VuotSongQuestion)
+admin.site.register(VuotSongAnswer)
